de126727736f4727c237cf2014d6a03d81b0c2ef
Anders Refsdal Olsen <andersro93@ros-nett.com>
2020-10-07 22:34:02 +0200

    Added missing tablename

--- src/Translaterr.Transman.Data/Entities/ApplicationLanguageEntity.cs
+++ src/Translaterr.Transman.Data/Entities/ApplicationLanguageEntity.cs
@@ -1,7 +1,8 @@
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Translaterr.Transman.Data.Entities
 {
+    [Table("ApplicationsLanguages")]
     public class ApplicationLanguageEntity
     {
         public int ApplicationId { get; set; }
